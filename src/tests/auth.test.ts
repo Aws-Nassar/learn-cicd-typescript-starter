06731ffaf3bfac34c5ee1e0209e67a22b60a635c
aws-nassar <aws.a.nasssar@gmail.com>
2026-03-21 23:35:32 +0200

    CH2: L1

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,32 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth.js";
+
+
+const IncomingHttpHeaders1 = {
+  authorization: "ApiKey 1202458Ftge",
+};
+
+const IncomingHttpHeaders2 = {
+  authorization: "",  
+};
+
+const IncomingHttpHeaders3 = {
+  authorization: "Bearer 1202458Ftge",
+};
+
+describe("ApiKey correctly formatted", () => {
+  test("ApiKey is correct", () => {
+    const result = getAPIKey(IncomingHttpHeaders1)
+    expect(result).toBe("1202458Ftge");
+  });
+
+  test("Empty header", () => {
+    const result = getAPIKey(IncomingHttpHeaders2)
+    expect(result).toBe(null);
+  });
+
+  test("Header not started with ApiKey ", () => {
+    const result = getAPIKey(IncomingHttpHeaders3)
+    expect(result).toBe(null);
+  });
+});
